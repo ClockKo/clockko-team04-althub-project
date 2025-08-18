4bd9d5c9239391e59a07bf4520384c893788a92c
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2025-08-18 07:31:04 +0100

    Removed ALB from Infra, as it is blocked due to AWS Basic plan

---
+++ backend/infra/ecs_service.tf
@@ -0,0 +1,17 @@
+# ECS Service on public subnets, tasks get public IP
+resource "aws_ecs_service" "backend" {
+  name            = "${var.project_name}-service"
+  cluster         = aws_ecs_cluster.clockko.id
+  task_definition = aws_ecs_task_definition.backend.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.public.id, aws_subnet.public_b.id]
+    security_groups  = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 200
+}

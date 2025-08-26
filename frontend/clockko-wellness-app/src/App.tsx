2fc013816fe17604da16df321142016815abbe58
Mariam Alli <mariam_oa@yahoo.com>
2025-08-26 19:06:42 +0100

    updated the routes

--- frontend/clockko-wellness-app/src/App.tsx
+++ frontend/clockko-wellness-app/src/App.tsx
@@ -1,15 +1,20 @@
-import { BrowserRouter } from "react-router-dom"
-import OnboardingRoutes from "./features/onboarding/routes/onboardingRoutes"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import OnboardingRoutes from "./features/onboarding/routes/onboardingRoutes";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-background p-8">
-        <OnboardingRoutes />
-      </div>
+      <Routes>
+        <Route path="/onboarding/*" element={<OnboardingRoutes />} />
+        {/* Add other routes like login, dashboard, etc. here */}
+        <Route path="*" element={<Navigate to="/onboarding" />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
+
+
+
 
--- frontend/clockko-wellness-app/src/features/onboarding/routes/onboardingRoutes.tsx
+++ frontend/clockko-wellness-app/src/features/onboarding/routes/onboardingRoutes.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { OnboardingLoginPage } from '../onboardingLoginPage'
-import { DashboardLayout } from '../../dashboard/dashboardLayout'
+import { DashboardLayout } from '../../../pages/dashboard/dashboard'
 import { Navigate } from 'react-router-dom'
 
 export default function OnboardingRoutes() {
   return (
     <Routes>
-      <Route path="/onboarding/login" element={<OnboardingLoginPage />} />
-      <Route path="/dashboard" element={<DashboardLayout />} />
-      <Route path="/" element={<Navigate to="/onboarding/login" />} />
+      <Route path="login" element={<OnboardingLoginPage />} />
+      <Route path="dashboard" element={<DashboardLayout />} />
+      <Route path="/" element={<Navigate to="login" />} />
     </Routes>
   )
 }

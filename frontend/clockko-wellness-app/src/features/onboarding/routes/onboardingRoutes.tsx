50fea8d9d56235a57826d2a1e4599fb8a580a8ae
Mariam Alli <mariam_oa@yahoo.com>
2025-09-01 18:02:32 +0100

    updated

--- frontend/clockko-wellness-app/src/features/onboarding/routes/onboardingRoutes.tsx
+++ frontend/clockko-wellness-app/src/features/onboarding/routes/onboardingRoutes.tsx
@@ -1,12 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
-// import { OnboardingLoginPage } from '../onboardingLoginPage'
 import { DashboardLayout } from '../../../pages/dashboard/dashboard'
 import { Navigate } from 'react-router-dom'
 
 export default function OnboardingRoutes() {
   return (
     <Routes>
-      {/* <Route path="login" element={<OnboardingLoginPage />} /> */}
       <Route path="dashboard" element={<DashboardLayout />} />
       <Route path="/" element={<Navigate to="login" />} />
     </Routes>

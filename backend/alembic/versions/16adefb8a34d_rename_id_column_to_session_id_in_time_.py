3c1318aa7acdcf37eebbe1090c913bb8238f06ee
maureen.onovae <maureenonovae@gmail.com>
2025-09-11 10:45:54 +0100

    fix: clean up backend authentication system

--- backend/alembic/versions/16adefb8a34d_rename_id_column_to_session_id_in_time_.py
+++ backend/alembic/versions/16adefb8a34d_rename_id_column_to_session_id_in_time_.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "16adefb8a34d"
-down_revision: Union[str, Sequence[str], None] = None
+down_revision: Union[str, Sequence[str], None] = "c14fa3262184"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 

2b298cf67d30435fdcd70d043752de7c3837bb5d
Maureen Onovae <maureenonovae@gmail.com>
2025-10-09 16:40:31 +0100

    feat: added the seed additional rooms for coworking rooms and created the challenge module

--- backend/app/services/coworkingservice.py
+++ backend/app/services/coworkingservice.py
@@ -6,7 +6,7 @@ from typing import List, Optional
 from fastapi import HTTPException
 
 from app.models.room import CoworkingRoom, RoomStatus
-# from app.models.room_participant import RoomParticipant
+  # from app.models.room_participant import RoomParticipant
 from app.models.coworking import RoomParticipant,RoomMessage
 from app.models.room_message import RoomMessageType
 from app.models.user import User
